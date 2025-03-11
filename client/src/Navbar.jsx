43ef1c5aa4cb530a3434866b0aed2e93c700d90c
Drogers1999 <daniel.rogers19@hotmial.com>
2025-03-11 20:46:34 +0000

    Added Navbar.jsx

---
+++ client/src/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <ol>
+                    <li>Home</li>
+                    <li>Prompt Helper</li>
+                    <li>Team</li>
+                    <li>About</li>
+                </ol>
+            </nav>
+        </header>
+    );
+};
