753033b45909775b29ff5e3149e24fbee1b1858b
unknown <varikvi@yahoo.com>
2025-03-17 23:40:55 +0100

    fix:navbar: solving conflicts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import InputSection from "./components/InputSection";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <>
       <InputSection />
+      <Navbar />
     </>
   );
 }
--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,13 +1,14 @@
+import './index.css'
 export default function Navbar() {
     return (
         <header>
-            <nav>
-                <ol>
-                    <li>Home</li>
-                    <li>Prompt Helper</li>
-                    <li>Team</li>
-                    <li>About</li>
-                </ol>
+            <nav className="nav-bar">
+                <ul>
+                    <li> <a href='#'>Home</a></li>
+                    <li><a href='#'>AI Tool</a></li>
+                    <li><a href='#'>About Us</a></li>
+                    <li><a href='#'>Contact Us</a></li>
+                </ul>
             </nav>
         </header>
     );
