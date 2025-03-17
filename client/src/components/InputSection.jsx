6147a5b86f78251a71db76a5b08e41e621c3667f
FlorSmile <flor.smile1@i.ua>
2025-03-17 23:06:47 +0200

    Add label and 5 input elements

--- client/src/components/InputSection.jsx
+++ client/src/components/InputSection.jsx
@@ -8,7 +8,23 @@ export default function InputSection() {
         Working on your Pentagram prompt
       </h2>
       <form className='p-10 md:p-20'>
-        <InputAreaBlock />
+        <InputAreaBlock lab='Person:' text='Explain your role...' />
+        <InputAreaBlock
+          lab='Context:'
+          text='Provide background information...'
+        />
+        <InputAreaBlock
+          lab='Task:'
+          text='Define what information you need...'
+        />
+        <InputAreaBlock
+          lab='Output:'
+          text='Define the format and details of the response...'
+        />
+        <InputAreaBlock
+          lab='Constraints:'
+          text='Limitations and guidelines that should be considered...'
+        />
         <button className='p-4 border rounded-2xl bg-black text-white duration-300 ease-in-out hover:bg-white hover:text-black hover:cursor-pointer'>
           Submit
         </button>
