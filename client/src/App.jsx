b14fd982b77691c9913558688bc6b61fc0f31ccf
FlorSmile <flor.smile1@i.ua>
2025-03-17 17:35:24 +0200

    Add Input section component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import InputAreaBlock from "./components/InputAreaBlock";
+import InputSection from "./components/InputSection";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <InputAreaBlock />
+      <InputSection />
       <div>
         <h1 class='text-3xl font-bold underline'>Hello world!</h1>
         <a href='https://vite.dev' target='_blank'>
---
+++ client/src/components/InputSection.jsx
@@ -0,0 +1,11 @@
+import InputAreaBlock from "./InputAreaBlock";
+
+/*Section 1*/
+export default function InputSection() {
+  return (
+    <form action='submit'>
+      <InputAreaBlock />
+      <button>Submit</button>
+    </form>
+  );
+}
