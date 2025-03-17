6a48cce3930867beed768115b5bd9a15556ee267
Drogers1999 <daniel.rogers19@hotmial.com>
2025-03-17 22:02:43 +0000

    Moved Navbar component to components folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import InputAreaBlock from "./components/InputAreaBlock";
-import Navbar from "./Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
