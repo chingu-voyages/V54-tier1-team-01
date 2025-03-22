a0a577a33030124c6cdf40c91377aee16b70c8db
unknown <varikvi@yahoo.com>
2025-03-22 17:35:44 +0100

    spike:mvpwflows: work on about scenario input

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <HeaderComponent />
-      <MainComponent />
+
     </>
 
   );
--- client/src/data/geminiPromptDescriptionInfo.js
+++ client/src/data/geminiPromptDescriptionInfo.js
@@ -1,9 +1,12 @@
 const PROMPTS = {
     test:``,
-    onMountScenarioPrompt: () => { 
+    onMountScenarioPrompt: (about) => { 
        /* basic prompt sent to gemini to formulate scenario */
+       if(about){
+        about = "The scenario should be about " + about;
+       }
       return `
-      You are a teacher teaching a student how to make prompts using the pentagram framework. Pentagram framework for making a prompt should consists in 5 parts: Persona (the role that the AI has to assume), a Task, the Context of the task, a desired Output format and some Constraints to the output. Your student is a user who is learning prompt engineering who don't know how to make prompts based on the pentagram framework. Your task is to provide the student with either a case or an scenario for which he / she should make a composite Pentagram-based prompt. Think of variety of scenarios in many fields (eg. music, engineering, software, policy, etc). The resulting prompt based on the scenario provided by you would then be sent to an AI which in turn will provide advice about the quality of that prompt. So it is a training that goes in that order:
+      You are a teacher teaching a student how to make prompts using the pentagram framework. Pentagram framework for making a prompt should consists in 5 parts: Persona (the role that the AI has to assume), a Task, the Context of the task, a desired Output format and some Constraints to the output. Your student is a user who is learning prompt engineering who don't know how to make prompts based on the pentagram framework. Your task is to provide the student with either a case or an scenario for which he / she should make a composite Pentagram-based prompt. ${about} The resulting prompt based on the scenario provided by you would then be sent to an AI which in turn will provide advice about the quality of that prompt. So it is a training that goes in that order:
       1. you provide the scenario and ask to make either one, several or a full prompt for that scenario based on pentagram framework
       2. the user follows your instructions, make the prompt and send that to you
       3. you provide a review, like a teacher, of the quality of the prompt based on the pentagram framework
