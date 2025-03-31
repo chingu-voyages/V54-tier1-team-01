5723c723f9be612cc3fd5d56c598cf0191e3a9dd
unknown <varikvi@yahoo.com>
2025-03-31 15:13:42 +0200

    feature:footer: add link to github; modif team list; modif name function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import HeaderComponent from "./components/Header";
 import MainComponent from "./components/MainComponent";
-import Footer from "./components/Footer";
+import FooterComponent from "./components/Footer";
 
 function App() {
   return (
     <>  
       <HeaderComponent />
       <MainComponent />
-      <Footer />
+      <FooterComponent />
     </>
 
   );
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function Footer() {
+export default function FooterComponent() {
    return (
        <footer className="text-black text-center py-4 text-lg">
             <p>&copy; {new Date().getFullYear()} My App. All rights reserved.</p>
-            <p>Developers: Evaristoc, Nadiia, VarshUI, Drogers1999</p>
+            <p>Developers: Evaristoc, Nadiia, Drogers1999; Scrum Master: Moshood</p>
+            <a href="https://github.com/chingu-voyages/V54-tier1-team-01">GitHub</a>
         </footer>
     );
 }
