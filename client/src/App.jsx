bdd2740a6720b61f8ed20f9bd8ca7db4df1da0d0
unknown <varikvi@yahoo.com>
2025-03-31 15:01:13 +0200

    feature:footer: init solving conflicts with App.jsx development

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import HeaderComponent from "./components/Header";
 import MainComponent from "./components/MainComponent";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
+    <>  
       <HeaderComponent />
       <MainComponent />
+      <Footer />
     </>
 
   );
