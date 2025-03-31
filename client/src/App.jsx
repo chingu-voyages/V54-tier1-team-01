e920dd597b8804bf5a4f75121dee6c0ff1c71aa5
unknown <varikvi@yahoo.com>
2025-04-01 00:25:01 +0200

    test:mvp: resolving all conflicts up to last development version

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,12 +7,8 @@ function App() {
   return (
     <>  
       <HeaderComponent />
-<<<<<<< HEAD
       <MainComponent />
       <FooterComponent />
-=======
-
->>>>>>> a0a577a (spike:mvpwflows: work on about scenario input)
     </>
 
   );
