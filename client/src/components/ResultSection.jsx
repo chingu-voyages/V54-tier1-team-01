c3d11bc1d8aa553e7f9e8b749b0ecaf14019e9ac
unknown <varikvi@yahoo.com>
2025-04-04 14:11:42 +0200

    test:mvp-styling01: styling footer and header

--- client/src/components/ResultSection.jsx
+++ client/src/components/ResultSection.jsx
@@ -3,7 +3,7 @@ import ReactMarkdown from 'react-markdown';
 export default function ResultSection(props) {
     return (
         <div className='resultsection gemini p-10 md:p-20 text-justify'>
-            {props.assessment? <ReactMarkdown>{props.assessment}</ReactMarkdown> : <h1 className="text-xl md:text-3xl font-bold text-red-300">Submit your prompt!</h1>}
+            {props.assessment? <ReactMarkdown>{props.assessment}</ReactMarkdown> : <h1 className="text-xl md:text-3xl font-bold text-red-300">Evaluate or Submit your Prompt!</h1>}
         </div>
     )
 }
