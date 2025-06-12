78ecfd9024f6b787cc39cd6ad1aeba4f1d2821ea
FlorSmile <flor.smile1@i.ua>
2025-06-12 18:41:47 +0300

    Change the gradient color to Hero page. Change the Header

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -3,9 +3,9 @@ import HeroImg from "../images/heroImg.png";
 
 export default function HeroSection() {
   return (
-    <section className='relative min-h-screen p-2 sm:p-4 md:p-9 bg-radial from-white via-neutral-200 to-neutral-50 overflow-hidden'>
+    <section className='relative min-h-screen px-2 sm:px-4 md:px-9 bg-radial from-white via-neutral-200 to-neutral-50 overflow-hidden'>
       <Header />
-      <div className='w-3/4 h-[calc(100vh-172px)] flex justify-center items-end sm:items-center'>
+      <div className='w-3/4 h-[calc(100vh-100px)] flex justify-center items-end sm:items-center'>
         <div className='w-3/4 flex flex-col items-center gap-7 text-center'>
           <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-2 bg-gradient-to-br from-(--primary-color) to-(--primary-dark-color)/80 bg-clip-text text-transparent'>
             Pentaprompt
